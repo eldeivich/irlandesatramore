29c2feef3c4c292f523bd4ca5718c95c58023927
eldeivich <eldey2017@outlook.com>
2026-07-08 16:32:52 +0200

    modificado seccion horario por cierre de miercoles 08072026

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ const weeklyHours = [
   { tavern: "17:00 - 00:30", kitchen: "Cocina de 19:30 a 23:00" },
   { tavern: "17:00 - 00:30", kitchen: "Cocina de 19:30 a 23:00" },
   { tavern: "17:00 - 00:30", kitchen: "Cocina de 19:30 a 23:00" },
-  { tavern: "17:00 - 00:30", kitchen: "Cocina de 19:30 a 23:00" },
+  { tavern: "Cerrado", kitchen: "Cocina cerrada" },
   { tavern: "17:00 - 00:30", kitchen: "Cocina de 19:30 a 23:00" },
   { tavern: "17:00 - 02:00", kitchen: "Cocina de 19:30 a 23:30" },
   { tavern: "17:00 - 02:00", kitchen: "Cocina de 19:30 a 23:30" },
